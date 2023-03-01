22651421b80afda7ae223c46d8057a3bb8da7d37
lneiz <yuntsevich@ymail.com>
2023-03-02 01:00:15 +0500

    54 - done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Количество строк массива? ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Количество столбцов массива? ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Минимаьное значение массива? ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Максимальное значение массива? ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows,cols];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return array;
+}
+
+int[,] SortUpToLowArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = j; k < array.GetLength(1); k++)
+            {
+                if(array[i,j] < array[i,k])
+                {
+                    int change = array[i,j];
+                    array[i,j] = array[i,k];
+                    array[i,k] = change;
+                }
+            }
+        }
+
+    }
+    return array;
+}
+
+void ShowArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+ShowArray(SortUpToLowArray(CreateRandom2dArray()));
+
