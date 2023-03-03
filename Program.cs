4b732bd5587ced7f06c8e2d3e6d233ce12b31b6d
lneiz <yuntsevich@ymail.com>
2023-03-04 02:05:23 +0500

    62 - done

--- Program.cs
+++ Program.cs
@@ -188,69 +188,140 @@
 //Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 //Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
-int[] UniqueCount(int count)
-{
-    Console.Write("Минимаьное значение массива? ");
-    int minValue = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Максимальное значение массива? ");
-    int maxValue = Convert.ToInt32(Console.ReadLine());
+// int[] UniqueCount(int count)
+// {
+//     Console.Write("Минимаьное значение массива? ");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Максимальное значение массива? ");
+//     int maxValue = Convert.ToInt32(Console.ReadLine());
 
-    if(maxValue - minValue <= count)
-    {
-        maxValue += count - (maxValue - minValue) + 1;
-    }
+//     if(maxValue - minValue <= count)
+//     {
+//         maxValue += count - (maxValue - minValue) + 1;
+//     }
 
-    int[] array = new int[count];
-    int j = 0;
+//     int[] array = new int[count];
+//     int j = 0;
 
-    for(int i = 0; i < count; i++)
-    {
-        do
-        {
-            j = new Random().Next(minValue, maxValue);
-        }
-        while(Array.IndexOf(array, j) != -1);
+//     for(int i = 0; i < count; i++)
+//     {
+//         do
+//         {
+//             j = new Random().Next(minValue, maxValue);
+//         }
+//         while(Array.IndexOf(array, j) != -1);
         
-        array[i] = j;
-    }
-    return array;
-}
+//         array[i] = j;
+//     }
+//     return array;
+// }
 
-int[,,] CreateRandom2dArray(int[] uniqueArray)
+// int[,,] CreateRandom2dArray(int[] uniqueArray)
+// {
+//     int[,,] array = new int[2, 2, 2];
+//     int count = 0;
+
+//     for(int i = 0; i < 2; i++)
+//     {
+//         for(int j = 0; j < 2; j++)
+//         {
+//             for(int k = 0; k < 2; k++)
+//             {
+//                 array[i,j,k] = uniqueArray[count];
+//                 count++;
+//             }
+//         }
+//     }
+//     return array;
+// }
+
+// void Show3dArray(int[,,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for(int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 Console.Write(array[i,j,k] + " ");
+//                 Console.Write("(" + i + " " + j + " " + k + ") ");
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
+
+// Show3dArray(CreateRandom2dArray(UniqueCount(8)));
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int[,] Create2dArray(int arraySize)
 {
-    int[,,] array = new int[2, 2, 2];
-    int count = 0;
+    int m = arraySize;
+    int[,] array = new int[arraySize,arraySize];
+    int k = 0;
+    int count = 1;
 
-    for(int i = 0; i < 2; i++)
+    array[arraySize / 2, arraySize / 2] = arraySize * arraySize;
+
+    for(int i = 0; i < arraySize / 2; i++)
     {
-        for(int j = 0; j < 2; j++)
+        for(int j = k; j < m - 1; j++)
         {
-            for(int k = 0; k < 2; k++)
+            if(count <= (arraySize * arraySize))
             {
-                array[i,j,k] = uniqueArray[count];
+                array[i,j] = count;
                 count++;
             }
         }
+
+        for(int j = k; j < m - 1; j++)
+        {
+            if(count <= (arraySize * arraySize))
+            {
+                array[j,m - 1] = count;
+                count++;
+            }
+        }
+
+        for(int j = m - 1; j > k; j--)
+        {
+            if(count <= (arraySize * arraySize))
+            {
+                array[m - 1, j] = count;
+                count++;
+            }
+        }
+
+        for(int j = m - 1; j > k; j--)
+        {
+            if(count <= (arraySize * arraySize))
+            {
+                array[j, i] = count;
+                count++;
+            }
+        }
+        m-=1;
+        k++;
     }
+
     return array;
 }
 
-void Show3dArray(int[,,] array)
+void ShowArray(int[,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {
-            for(int k = 0; k < array.GetLength(2); k++)
-            {
-                Console.Write(array[i,j,k] + " ");
-                Console.Write("(" + i + " " + j + " " + k + ") ");
-            }
-            Console.WriteLine();
+            Console.Write(array[i,j] + "   ");
         }
+        Console.WriteLine();
     }
+    Console.WriteLine();
 }
 
-Show3dArray(CreateRandom2dArray(UniqueCount(8)));
+Console.Write("Укажите размер массива? ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
 
-
+ShowArray(Create2dArray(arraySize));
